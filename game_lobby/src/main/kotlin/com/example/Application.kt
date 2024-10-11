073251490552ece9cbff4da5c13c7284d349d752
muffindud <ccatlabuga2002@gmail.com>
2024-10-11 13:29:14 +0300

    setup serialization lib

--- game_lobby/src/main/kotlin/com/example/Application.kt
+++ game_lobby/src/main/kotlin/com/example/Application.kt
@@ -10,26 +10,39 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.runBlocking
 
-//val game_lobby_port = System.getenv("GAME_LOBBY_PORT").toInt()
-//val user_manager_host = System.getenv("USER_MANAGER_HOST")
-//val user_manager_port = System.getenv("USER_MANAGER_PORT").toInt()
-//val jwt_internal_secret = System.getenv("JWT_INTERNAL_SECRET")
+var game_lobby_port: Int = 8000
+var user_manager_host: String = "user_manager"
+var user_manager_port: Int = 3000
+var jwt_internal_secret: String = "internal_sike"
+var jwt_user_secret: String = "sike"
 
 fun main(args: Array<String>) {
-    // embeddedServer(
-    //     Netty,
-    //     port = game_lobby_port,
-    //     host = "0.0.0.0",
-    //     module = Application::module
-    // )
-    //     .start(wait = true)
-    runBlocking {
-        println(getResponse().bodyAsText())
-        client.close()
+    try {
+        game_lobby_port = System.getenv("GAME_LOBBY_PORT").toInt()
+        user_manager_host = System.getenv("USER_MANAGER_HOST")
+        user_manager_port = System.getenv("USER_MANAGER_PORT").toInt()
+        jwt_internal_secret = System.getenv("JWT_INTERNAL_SECRET")
+        jwt_user_secret = System.getenv("JWT_USER_SECRET")
+    } catch (e: Exception) {
+        game_lobby_port = 8000
+        user_manager_host = "user_manager"
+        user_manager_port = 3000
+        jwt_internal_secret = "internal_sike"
+        jwt_user_secret = "sike"
     }
+
+    embeddedServer(
+        Netty,
+        port = game_lobby_port,
+        host = "0.0.0.0",
+        module = Application::module
+    )
+        .start(wait = true)
 }
 
-// fun Application.module() {
-//     configureSockets()
-//     configureMonitoring()
-// }
+fun Application.module() {
+    configureSockets()
+    configureMonitoring()
+    configureRouting()
+    configureSecurity(jwt_user_secret, jwt_internal_secret)
+}

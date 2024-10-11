0535912783df44d72d90551e43154382d787d69e
muffindud <ccatlabuga2002@gmail.com>
2024-10-11 13:28:27 +0300

    setup connect ws endpoint

--- game_lobby/src/main/kotlin/com/example/plugins/Sockets.kt
+++ game_lobby/src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,11 +1,14 @@
 package com.example.plugins
 
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import java.time.*
+import kotlinx.serialization.protobuf.*
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -14,6 +17,8 @@ fun Application.configureSockets() {
         timeout = Duration.ofSeconds(15)
         maxFrameSize = Long.MAX_VALUE
         masking = false
+        contentConverter = KotlinxWebsocketSerializationConverter(ProtoBuf)
+
     }
     routing {
         webSocket("/echo") {
@@ -23,6 +28,15 @@ fun Application.configureSockets() {
                 val receivedText = frame.readText()
                 send("You said: $receivedText")
             }
+        }
+        webSocket("/connect/{user_token}/{game_id}") {
+            val userToken = call.parameters["user_token"]
+            val gameId = call.parameters["game_id"]
+            if (userToken == null || gameId == null) {
+                close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT, "Invalid parameters"))
+                return@webSocket
+            }
+            send("Connected to game $gameId")
         }
     }
 }

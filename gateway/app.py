fabc81d9da919e5eab1d212416addf934a8d72e0
buffaloss <beatricia.golban@isa.utm.md>
2024-10-22 21:06:48 +0300

    add basic flask ws

--- gateway/app.py
+++ gateway/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sock import Sock
 from os import environ
 
 app = Flask(__name__)
+sock = Sock(app)
 app.config['USER_MANAGER_HOST'] = environ.get('USER_MANAGER_HOST')
 app.config['USER_MANAGER_PORT'] = environ.get('USER_MANAGER_PORT')
+app.config['GAME_LOBBY_HOST'] = environ.get('GAME_LOBBY_HOST')
+app.config['GAME_LOBBY_PORT'] = environ.get('GAME_LOBBY_PORT')
 
 import routes.user_manager
+import routes.game_lobby
--- gateway/routes/game_lobby.py
+++ gateway/routes/game_lobby.py
@@ -1,1 +1,12 @@
-from app import app
+from app import app, sock
+
+
+game_lobby_url = app.config['GAME_LOBBY_HOST'] + ':' + app.config['GAME_LOBBY_PORT']
+
+
+@sock.route('/connect/<int:id>')
+def connect(ws, id):
+    # TODO: Redirect to game lobby
+    ws.send("Connected to " + str(id))
+    while True:
+        ws.send("Received: " + ws.receive())

841e5b63a0e988eebecc0a10cb33f003a13318f1
Corneliu <73847818+muffindud@users.noreply.github.com>
2024-10-23 21:51:08 +0300

    Feat/task timeouts (#12)
    * fix game lobby health check
    
    * add task timeout to game lobby
    
    * fix http gateway routes
    
    * increase default timeouts
    
    * add task timeout to user manager
    
    * add logger to exchange service
    
    * restructure game lobby logging

--- game_lobby/src/main/kotlin/com/example/plugins/Monitoring.kt
+++ game_lobby/src/main/kotlin/com/example/plugins/Monitoring.kt
@@ -2,14 +2,24 @@ package com.example.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.origin
 import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import org.slf4j.event.*
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
-        level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
+        format { call ->
+            val remoteHost = call.request.origin.remoteHost
+            val method = call.request.httpMethod.value
+            val uri = call.request.uri
+            val protocol = call.request.httpVersion
+            val status = call.response.status()?.value ?: "-"
+            val userAgent = call.request.headers["User-Agent"] ?: "unknown"
+            val dateFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH)
+            val date = dateFormat.format(Date())
+            "$remoteHost - - [$date] \"$method $uri $protocol\" $status - \"$userAgent\""
+        }
     }
 }
--- user_manager/controllers/user.controller.js
+++ user_manager/controllers/user.controller.js
@@ -4,267 +4,162 @@ const User = db.users;
 const Op = db.Sequelize.Op;
 const { generateUserToken, verifyUserToken, verifyInternalToken } = require('../utils/authJwt');
 
-User.secureCreate = (req, res) => {
-    password_hash = bcrypt.hashSync(req.body.password, 8);
+User.secureCreate = async (req, res) => {
+    try {
+        const password_hash = bcrypt.hashSync(req.body.password, 8);
+        const user = {
+            username: req.body.username,
+            password_hash: password_hash,
+            email: req.body.email,
+            balance: req.body.balance
+        };
 
-    const user = {
-        username: req.body.username,
-        password_hash: password_hash,
-        email: req.body.email,
-        balance: req.body.balance
+        const existingUser = await User.findOne({ where: { username: user.username } });
+        if (existingUser) {
+            return res.status(409).send({ message: 'Username already exists.' });
+        }
+
+        const newUser = await User.create(user);
+        const token = generateUserToken(newUser.username);
+        res.send({ token: token });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ message: err.message || 'Some error occurred while creating the User.' });
     }
-
-    User.findOne({
-        where: {
-            username: user.username
-        }
-    })
-        .then(data => {
-            if (data) {
-                return res.status(409).send({ message: 'Username already exists.' });
-            }
-
-            User.create(user)
-                .then(data => {
-                    const token = generateUserToken(data.username);
-                    res.send({ token: token });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).send({
-                        message: err.message || 'Some error occurred while creating the User.'
-                    });
-                });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send({ message: err.message });
-        });
-}
-
-User.secureLogin = (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({
-        where: {
-            username: username
-        }
-    })
-        .then(data => {
-            if (!data) {
-                return res.status(404).send({ message: 'User Not found.' });
-            }
-
-            const passwordIsValid = bcrypt.compareSync(password, data.password_hash);
-
-            if (!passwordIsValid) {
-                return res.status(401).send({
-                    accessToken: null,
-                    message: 'Invalid Password!'
-                });
-            }
-
-            const token = generateUserToken(data.username);
-            res.status(200).send({ token: token });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send({ message: err.message });
-        });
 };
 
-User.secureFind = (req, res) => {
+User.secureLogin = async (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const user = await User.findOne({ where: { username: username } });
+        if (!user) {
+            return res.status(404).send({ message: 'User Not found.' });
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password_hash);
+        if (!passwordIsValid) {
+            return res.status(401).send({ message: 'Invalid Password!' });
+        }
+
+        const token = generateUserToken(user.username);
+        res.status(200).send({ token: token });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ message: err.message });
+    }
+};
+
+User.secureFind = async (req, res) => {
     if (!req.get('Authorization')) {
         return res.status(401).send({ message: 'Unauthorized' });
     }
 
-    const token = req.get('Authorization').split(' ')[1];
-    const decoded = verifyUserToken(token);
-    const user = User.findOne({
-        where: {
-            username: decoded.username
-        }
-    })
-        .then(data => {
-            if (!data) {
-                return res.status(404).send({ message: 'User Not found.' });
-            }
+    try {
+        const token = req.get('Authorization').split(' ')[1];
+        const decoded = verifyUserToken(token);
 
-            res.status(200).send({
-                username: data.username,
-                email: data.email,
-                balance: data.balance
-            });
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
+        const user = await User.findOne({ where: { username: decoded.username } });
+        if (!user) {
+            return res.status(404).send({ message: 'User Not found.' });
+        }
+
+        res.status(200).send({
+            username: user.username,
+            email: user.email,
+            balance: user.balance
         });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ message: err.message });
+    }
 };
 
-User.secureTransfer = (req, res) => {
+User.secureTransfer = async (req, res) => {
     if (!req.get('Authorization')) {
         return res.status(401).send({ message: 'Unauthorized' });
     }
 
-    const token = req.get('Authorization').split(' ')[1];
-    const decoded = verifyUserToken(token);
-    const sender = decoded.username;
-    const receiver = req.body.username;
-    const amount = req.body.amount;
+    try {
+        const token = req.get('Authorization').split(' ')[1];
+        const decoded = verifyUserToken(token);
+        const sender = decoded.username;
+        const receiver = req.body.username;
+        const amount = req.body.amount;
 
-    // get the sender from the database
-    User.findOne({
-        where: {
-            username: sender
+        const senderData = await User.findOne({ where: { username: sender } });
+        if (!senderData) {
+            return res.status(404).send({ message: 'Sender Not found.' });
         }
-    })
-        .then(data => {
-            if (!data) {
-                return res.status(404).send({ message: 'Sender Not found.' });
-            }
+        if (senderData.balance < amount) {
+            return res.status(400).send({ message: 'Insufficient balance.' });
+        }
 
-            if (data.balance < amount) {
-                return res.status(400).send({ message: 'Insufficient balance.' });
-            }
+        const receiverData = await User.findOne({ where: { username: receiver } });
+        if (!receiverData) {
+            return res.status(404).send({ message: 'Receiver Not found.' });
+        }
 
-            User.findOne({
-                where: {
-                    username: receiver
-                }
-            })
-                .then(data => {
-                    if (!data) {
-                        return res.status(404).send({ message: 'Receiver Not found.' });
-                    }
+        await User.update({ balance: receiverData.balance + amount }, { where: { username: receiver } });
+        await User.update({ balance: senderData.balance - amount }, { where: { username: sender } });
 
-                    User.update({
-                        balance: data.balance + amount
-                    }, {
-                        where: {
-                            username: receiver
-                        }
-                    })
-                        .then(num => {
-                            if (num == 1) {
-                                User.update({
-                                    balance: data.balance - amount
-                                }, {
-                                    where: {
-                                        username: sender
-                                    }
-                                })
-                                    .then(num => {
-                                        if (num == 1) {
-                                            // TODO: Add transfer to neo4j database
-                                            return res.status(200).send({ message: 'Transfer successful.' });
-                                        }
-                                    })
-                                    .catch(err => {
-                                        res.status(500).send({ message: err.message });
-                                    });
-                            }
-                        })
-                        .catch(err => {
-                            res.status(500).send({ message: err.message });
-                        });
-                })
-                .catch(err => {
-                    res.status(500).send({ message: err.message });
-                });
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
-        });
+        res.status(200).send({ message: 'Transfer successful.' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ message: err.message });
+    }
 };
 
-User.internalBalance = (req, res) => {
+User.internalBalance = async (req, res) => {
     if (!req.get('Authorization')) {
         return res.status(401).send({ message: 'Unauthorized' });
     }
 
-    const token = req.get('Authorization').split(' ')[1];
+    try {
+        const token = req.get('Authorization').split(' ')[1];
+        const decoded = verifyInternalToken(token);
 
-    if (!token) {
-        return res.status(401).send({ message: 'Unauthorized' });
-    }
-
-    const decoded = verifyInternalToken(token);
-
-    if (!decoded) {
-        return res.status(401).send({ message: 'Unauthorized' });
-    }
-
-    const user_id = req.params.user_id;
-    User.findOne({
-        where: {
-            id: user_id
+        if (!decoded) {
+            return res.status(401).send({ message: 'Unauthorized' });
         }
-    })
-        .then(data => {
-            if (!data) {
-                return res.status(404).send({ message: 'User Not found.' });
-            }
 
-            res.status(200).send({
-                balance: data.balance
-            });
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
-        });
+        const user = await User.findOne({ where: { id: req.params.user_id } });
+        if (!user) {
+            return res.status(404).send({ message: 'User Not found.' });
+        }
+
+        res.status(200).send({ balance: user.balance });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ message: err.message });
+    }
 };
 
-User.internalUpdateBalance = (req, res) => {
+User.internalUpdateBalance = async (req, res) => {
     if (!req.get('Authorization')) {
         return res.status(401).send({ message: 'Unauthorized' });
     }
 
-    const token = req.get('Authorization').split(' ')[1];
+    try {
+        const token = req.get('Authorization').split(' ')[1];
+        const decoded = verifyInternalToken(token);
 
-    if (!token) {
-        return res.status(401).send({ message: 'Unauthorized' });
-    }
-
-    const decoded = verifyInternalToken(token);
-
-    if (!decoded) {
-        return res.status(401).send({ message: 'Unauthorized' });
-    }
-
-    const user_id = req.params.user_id;
-    const balance = req.body.balance;
-
-    User.findOne({
-        where: {
-            id: user_id
+        if (!decoded) {
+            return res.status(401).send({ message: 'Unauthorized' });
         }
-    })
-        .then(data => {
-            if (!data) {
-                return res.status(404).send({ message: 'User Not found.' });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
-        });
 
-    User.update({
-        balance: balance
-    }, {
-        where: {
-            id: user_id
+        const user = await User.findOne({ where: { id: req.params.user_id } });
+        if (!user) {
+            return res.status(404).send({ message: 'User Not found.' });
         }
-    })
-        .then(num => {
-            if (num == 1) {
-                // TODO: Add update balance to neo4j database
-                res.status(200).send({ message: 'Balance updated successfully.' });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
-        });
+
+        await User.update({ balance: req.body.balance }, { where: { id: req.params.user_id } });
+        res.status(200).send({ message: 'Balance updated successfully.' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ message: err.message });
+    }
 };
 
 module.exports = User;

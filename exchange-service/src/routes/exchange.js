9e6396bf44cad372e47e4b719be07b963e218a64
tris <89996471+buffaloss@users.noreply.github.com>
2024-10-23 23:55:02 +0300

    Feat/exchange cache (#13)
    * implement custom exchange cache
    
    * dockerize exchange cache
    
    * integrate exchange chache into exchange service

--- exchange-service/src/routes/exchange.js
+++ exchange-service/src/routes/exchange.js
@@ -25,23 +25,54 @@ router.get("/exchange-rate", async (req, res) => {
   }
 
   try {
+    const apiUrl = `http://${process.env.EXCHANGE_CACHE_HOST}:${process.env.EXCHANGE_CACHE_PORT}/?baseCurrency=${baseCurrency}&targetCurrency=${targetCurrency}`;
+    const response = await requestWithTimeout(apiUrl);
+    const exchangeRate = response.data.rate;
+
+    return res.json({
+      baseCurrency,
+      targetCurrency,
+      exchangeRate,
+    });
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      console.log(`Exchange rate for ${baseCurrency} to ${targetCurrency} not found in cache. Fetching from currency-api...`);
+    } else {
+      return res
+        .status(500)
+        .json({ message: "Error fetching exchange rate." + error.message });
+    }
+  }
+
+  try {
     const apiUrl = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${baseCurrency}.json`;
     const response = await requestWithTimeout(apiUrl);
     const rates = response.data[baseCurrency];
 
     if (rates && rates[targetCurrency]) {
       const exchangeRate = rates[targetCurrency];
-      res.json({
+
+      // store the exchange rate in the exchange-cache service
+      const cacheUrl = `http://${process.env.EXCHANGE_CACHE_HOST}:${process.env.EXCHANGE_CACHE_PORT}/`;
+      try {
+        await axios.post(cacheUrl, {
+          [baseCurrency]: rates,
+        });
+      } catch (error) {
+        console.error("Error storing exchange rate in cache.", error.message);
+      }
+
+      return res.json({
         baseCurrency,
         targetCurrency,
         exchangeRate,
       });
     } else {
-      res.status(404).json({ message: "Target currency not found." });
+      return res.status(404).json({ message: "Target currency not found." });
     }
   } catch (error) {
     console.error(error);
-    res
+    return res
       .status(500)
       .json({ message: "Error fetching exchange rate." + error.message });
   }

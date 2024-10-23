d36dd1d8f74cc07043a93199d699fda9e5ae6008
buffaloss <beatricia.golban@isa.utm.md>
2024-10-23 12:42:24 +0300

    add lobby ws reroute

--- gateway/routes/game_lobby.py
+++ gateway/routes/game_lobby.py
@@ -1,12 +1,25 @@
 from app import app, sock
+from websocket import create_connection
+from flask_sock import Server
+from flask import request
 
 
 game_lobby_url = app.config['GAME_LOBBY_HOST'] + ':' + app.config['GAME_LOBBY_PORT']
 
 
 @sock.route('/connect/<int:id>')
-def connect(ws, id):
-    # TODO: Redirect to game lobby
-    ws.send("Connected to " + str(id))
+def connect(client_sock: Server, id):
+    lobby_sock = create_connection(
+        f'ws://{game_lobby_url}/connect/{id}',
+        header={'Authorization': request.headers.get('Authorization')}
+    )
+
     while True:
-        ws.send("Received: " + ws.receive())
+        message = client_sock.receive()
+        print(f'Client {id} sent: {message}')
+        lobby_sock.send(message)
+        response = lobby_sock.recv()
+        print(f'Lobby sent: {response}')
+        client_sock.send(response)
+
+    lobby_sock.close()

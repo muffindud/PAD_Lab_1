f36740d09282d0c6fb8c461940caed3ffb6a1ab2
muffindud <ccatlabuga2002@gmail.com>
2024-10-23 20:15:45 +0300

    fix http gateway routes

--- gateway/routes/user_manager.py
+++ gateway/routes/user_manager.py
@@ -1,5 +1,5 @@
 from app import app
-from requests import post, get
+from httpx import AsyncClient
 from quart import request
 
 
@@ -8,39 +8,49 @@ user_manager_url = app.config['USER_MANAGER_HOST'] + ':' + app.config['USER_MANA
 
 @app.route('/register', methods=['POST'])
 async def register():
-    response = post(
-        url='http://' + user_manager_url + '/register',
-        json=request.json
-    )
+    async with AsyncClient() as client:
+        response = await client.request(
+            method='POST',
+            url=f'http://{user_manager_url}/register',
+            headers=request.headers,
+            json=await request.get_json()
+        )
 
     return response.text, response.status_code
 
 
 @app.route('/login', methods=['GET'])
 async def login():
-    response = get(
-        url='http://' + user_manager_url + '/login',
-        json=request.json
-    )
+    async with AsyncClient() as client:
+        response = await client.request(
+            method='GET',
+            url=f'http://{user_manager_url}/login',
+            json=await request.get_json()
+        )
 
     return response.text, response.status_code
 
 
 @app.route('/profile', methods=['GET'])
 async def profile():
-    response = get(
-        url='http://' + user_manager_url + '/profile',
-        headers=request.headers
-    )
+    async with AsyncClient() as client:
+        response = await client.request(
+            method='GET',
+            url=f'http://{user_manager_url}/profile',
+            headers={'Authorization': request.headers['Authorization']}
+        )
 
     return response.text, response.status_code
 
 
 @app.route('/transfer', methods=['POST'])
 async def transfer():
-    response = post(
-        url='http://' + user_manager_url + '/transfer',
-        json=request.json
-    )
+    async with AsyncClient() as client:
+        response = await client.request(
+            method='POST',
+            url=f'http://{user_manager_url}/transfer',
+            headers={'Authorization': request.headers['Authorization']},
+            json=await request.get_json()
+        )
 
     return response.text, response.status_code

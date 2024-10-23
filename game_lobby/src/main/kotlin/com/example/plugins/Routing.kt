c4c82e7671863b308c263bf3b9cfdc387b6c2036
muffindud <ccatlabuga2002@gmail.com>
2024-10-23 18:09:50 +0300

    add task timeout to game lobby

--- game_lobby/src/main/kotlin/com/example/plugins/Routing.kt
+++ game_lobby/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,8 +1,17 @@
 package com.example.plugins
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withTimeout
+
+suspend fun <T> executeWithTimeout(block: suspend () -> T): T {
+    return withTimeout(3000) {
+        block()
+    }
+}
 
 fun Application.configureRouting() {
     routing {
@@ -11,7 +20,15 @@ fun Application.configureRouting() {
         }
 
         get("/health") {
-            call.respond("{\"status\": \"healthy\"}")
+            // call.respond("{\"status\": \"healthy\"}")
+            try {
+                val result = executeWithTimeout {
+                    "{\"status\": \"healthy\"}"
+                }
+                call.respond(result)
+            } catch (e: TimeoutCancellationException) {
+                call.respond(HttpStatusCode.RequestTimeout, "{\"status\": \"unhealthy\"}")
+            }
         }
     }
 }

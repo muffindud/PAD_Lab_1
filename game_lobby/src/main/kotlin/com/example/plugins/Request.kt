6ccb1688cbbb2d539be01ad03450bf99ebe70a5e
muffindud <ccatlabuga2002@gmail.com>
2024-10-26 00:08:42 +0300

    integrate sd in game lobby service

--- game_lobby/src/main/kotlin/com/example/plugins/Request.kt
+++ game_lobby/src/main/kotlin/com/example/plugins/Request.kt
@@ -1,12 +1,45 @@
 package com.example.plugins
 
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.Serializable
+import java.net.InetAddress
 
-val client = HttpClient(CIO)
-suspend fun getResponse(): HttpResponse
+@Serializable
+data class ServiceDiscoveryResponse(val service_id: String, val message: String)
+
+val client = HttpClient(CIO) {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true // in case the JSON response has more fields than your data class
+        })
+    }
+}
+
+suspend fun registerService(serviceDiscoveryUrl: String, port: Int): String
 {
-    return client.get("http://example.com")
+    println("Registering service as ${InetAddress.getLocalHost().hostName}:$port")
+    val response: HttpResponse = client.post(urlString = serviceDiscoveryUrl) {
+        contentType(ContentType.Application.Json)
+        setBody(
+            """
+            {
+                "service_name": "Game Lobby",
+                "host": "${InetAddress.getLocalHost().hostName}",
+                "port": "$port"
+            }
+            """.trimIndent()
+        )
+    }
+
+    val responseJson: ServiceDiscoveryResponse = response.body()
+
+    return responseJson.service_id
 }

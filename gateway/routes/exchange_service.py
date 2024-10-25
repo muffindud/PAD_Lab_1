c739c7aa8ee7ea5e303fb1b669d85fa9274dca39
muffindud <ccatlabuga2002@gmail.com>
2024-10-26 02:33:17 +0300

    fix refference errors

--- gateway/routes/exchange_service.py
+++ gateway/routes/exchange_service.py
@@ -1,4 +1,4 @@
-from app import app, service_registry
+from app import app, get_service_registry
 from httpx import AsyncClient
 from quart import request, jsonify
 from quart_rate_limiter import rate_limit
@@ -7,19 +7,24 @@ from json import loads
 
 round_robin_index = 0
 def get_round_robin_exchange_service() -> str:
+    global round_robin_index
+    service_registry = get_service_registry('Exchange Service')
+
     # If there are no exchange services, return None
-    if len(service_registry['Exchange Service']) == 0:
+    if len(service_registry) == 0:
         return None
 
     # If the round robin index is out of bounds, reset it
-    if round_robin_index >= len(service_registry['Exchange Service']):
+    if round_robin_index >= len(service_registry):
         round_robin_index = 0
 
+    serv_id = list(service_registry.keys())[round_robin_index]
+
     # Get the next exchange service host
-    host = service_registry['Exchange Service'][round_robin_index]
+    host = service_registry[serv_id]
 
     # Increment the round robin index and keep it within bounds
-    round_robin_index = (round_robin_index + 1) % len(service_registry['Exchange Service'])
+    round_robin_index = (round_robin_index + 1) % len(service_registry)
 
     return host
 
@@ -39,7 +44,12 @@ async def exchange():
                 url=f'http://{host}/exchange-rate/?baseCurrency={request.args.get("baseCurrency")}&targetCurrency={request.args.get("targetCurrency")}'
             )
 
-        return jsonify(loads(response.text)), response.status_code
+        try:
+            r = jsonify(loads(response.text)), response.status_code
+        except Exception as e:
+            r = response.text, response.status_code
+
+        return r
 
     else:
         return jsonify({'error': 'Method not allowed'}), 405

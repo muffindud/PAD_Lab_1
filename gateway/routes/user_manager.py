163601883f98065ca004f2f147c3505c1dba47b8
muffindud <ccatlabuga2002@gmail.com>
2024-10-26 01:22:45 +0300

    add round robin to user manager

--- gateway/routes/user_manager.py
+++ gateway/routes/user_manager.py
@@ -1,20 +1,41 @@
-from app import app
+from app import app, service_registry
 from httpx import AsyncClient
 from quart import request, jsonify
 from quart_rate_limiter import rate_limit
 from json import loads
 
 
-user_manager_url = app.config['USER_MANAGER_HOST'] + ':' + app.config['USER_MANAGER_PORT']
+round_robin_index = 0
+def get_round_robin_exchange_service() -> str:
+    # If there are no user manager services, return None
+    if len(service_registry['User Manager']) == 0:
+        return None
+
+    # If the round robin index is out of bounds, reset it
+    if round_robin_index >= len(service_registry['User Manager']):
+        round_robin_index = 0
+
+    # Get the next user manager service host
+    host = service_registry['User Manager'][round_robin_index]
+
+    # Increment the round robin index and keep it within bounds
+    round_robin_index = (round_robin_index + 1) % len(service_registry['User Manager'])
+
+    return host
 
 
 @app.route('/register', methods=['POST'])
 @rate_limit(app.config['RATE_LIMIT'], app.config['RATE_LIMIT_PERIOD'])
 async def register():
+    host = get_round_robin_exchange_service()
+
+    if host is None:
+        return jsonify({'error': 'No user manager services available'}), 503
+
     async with AsyncClient(timeout=30.0) as client:
         response = await client.request(
             method='POST',
-            url=f'http://{user_manager_url}/register',
+            url=f'http://{host}/register',
             json=await request.get_json()
         )
 
@@ -24,10 +45,15 @@ async def register():
 @app.route('/login', methods=['GET'])
 @rate_limit(app.config['RATE_LIMIT'], app.config['RATE_LIMIT_PERIOD'])
 async def login():
+    host = get_round_robin_exchange_service()
+
+    if host is None:
+        return jsonify({'error': 'No user manager services available'}), 503
+
     async with AsyncClient(timeout=30.0) as client:
         response = await client.request(
             method='GET',
-            url=f'http://{user_manager_url}/login',
+            url=f'http://{host}/login',
             json=await request.get_json()
         )
 
@@ -37,10 +63,15 @@ async def login():
 @app.route('/profile', methods=['GET'])
 @rate_limit(app.config['RATE_LIMIT'], app.config['RATE_LIMIT_PERIOD'])
 async def profile():
+    host = get_round_robin_exchange_service()
+
+    if host is None:
+        return jsonify({'error': 'No user manager services available'}), 503
+
     async with AsyncClient(timeout=30.0) as client:
         response = await client.request(
             method='GET',
-            url=f'http://{user_manager_url}/profile',
+            url=f'http://{host}/profile',
             headers={'Authorization': request.headers['Authorization']}
         )
 
@@ -50,10 +81,15 @@ async def profile():
 @app.route('/transfer', methods=['POST'])
 @rate_limit(app.config['RATE_LIMIT'], app.config['RATE_LIMIT_PERIOD'])
 async def transfer():
+    host = get_round_robin_exchange_service()
+
+    if host is None:
+        return jsonify({'error': 'No user manager services available'}), 503
+
     async with AsyncClient(timeout=30.0) as client:
         response = await client.request(
             method='POST',
-            url=f'http://{user_manager_url}/transfer',
+            url=f'http://{host}/transfer',
             headers={'Authorization': request.headers['Authorization']},
             json=await request.get_json()
         )
@@ -64,10 +100,15 @@ async def transfer():
 @app.route('/transfer', methods=['GET'])
 @rate_limit(app.config['RATE_LIMIT'], app.config['RATE_LIMIT_PERIOD'])
 async def get_transfers():
+    host = get_round_robin_exchange_service()
+
+    if host is None:
+        return jsonify({'error': 'No user manager services available'}), 503
+
     async with AsyncClient(timeout=30.0) as client:
         response = await client.request(
             method='GET',
-            url=f'http://{user_manager_url}/transfer',
+            url=f'http://{host}/transfer',
             headers={'Authorization': request.headers['Authorization']}
         )
 

c6d9f3ee976f38f74f4df272e370303d3b900a8e
muffindud <ccatlabuga2002@gmail.com>
2024-11-20 10:38:48 +0200

    add route for port retreive

--- service-discovery/routes/service_discovery.py
+++ service-discovery/routes/service_discovery.py
@@ -1,5 +1,9 @@
 from app import app, services
 from flask import request, jsonify
+from os import environ
+
+
+game_lobby_free_port: int = int(environ['GAME_LOBBY_PORT'])
 
 
 @app.route('/discovery', methods=['GET', 'POST'])
@@ -44,6 +48,21 @@ def discovery():
 
         # Return the service_id
         return jsonify({'message': 'Service added successfully', 'service_id': service_id}), 201
+
+    else:
+        return jsonify({
+            'message': 'Invalid request'
+        })
+
+
+
+@app.route('/discovery/get_lobby_port', methods=['GET'])
+def get_lobby_port():
+    if request.method == 'GET':
+        global game_lobby_free_port
+        port = game_lobby_free_port
+        game_lobby_free_port += 1
+        return jsonify({'port': port})
 
     else:
         return jsonify({

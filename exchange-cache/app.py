093b05e6ed45eea95d11719c9ad75151203e1519
muffindud <ccatlabuga2002@gmail.com>
2024-12-10 21:06:27 +0200

    add ring retreival

--- exchange-cache/app.py
+++ exchange-cache/app.py
@@ -1,10 +1,12 @@
 from quart import Quart, request, jsonify
 from datetime import datetime
-from asyncio import create_task
+from asyncio import create_task, sleep
 from httpx import AsyncClient
 from socket import gethostname
 from os import environ
 from threading import Lock
+from hashlib import sha256
+from bisect import bisect_right
 
 
 RATE_LIFETIME = 60 * 60  # 1 hour
@@ -37,14 +39,50 @@ ring_updater_task = None
 cache_id = None
 cache_ring = {}
 cache_ring_lock = Lock()
+cache_ids = []
+cache_ids_lock = Lock()
+
+
+async def recalibrate_ring(new_cache_ring):
+    global cache_ring
+    global cache_ring_lock
+    global cache_ids
+    global cache_ids_lock
+
+    old_ring = None
+    old_ids = None
+
+    async with cache_ring_lock:
+        old_ring = cache_ring
+        cache_ring = {int(cache_id, 16): cache_info for cache_id, cache_info in new_cache_ring.items()}
+
+    async with cache_ids_lock:
+        old_ids = cache_ids
+        cache_ids = list([int(cache_id, 16) for cache_id in cache_ring.keys()])
+        cache_ids.sort()
+
+    # Handle removed nodes (check if the held data is from a lower cache_id)
+    # TODO
+
+    # Handle new nodes
+    # TODO
+
 
 
 async def update_ring():
     global cache_ring
     global cache_ring_lock
 
+    sleep(5)
+
     while True:
-        ...
+        async with AsyncClient() as client:
+            response = await client.get(SERIVCE_DISCOVERY_URL)
+            new_cache_ring = response.json()
+            if new_cache_ring != cache_ring:
+                await recalibrate_ring(new_cache_ring)
+
+        await sleep(CACHE_RING_UPDATE_INTERVAL)
 
 
 @app.before_serving
@@ -58,7 +96,7 @@ async def startup():
             })
 
         global cache_id
-        cache_id = response.json()["cache_id"]
+        cache_id = int(response.json()["cache_id"], 16)
 
     global ring_updater_task
     ring_updater_task = create_task(update_ring())

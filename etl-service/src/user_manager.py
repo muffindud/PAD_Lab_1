54405fe0df310b42a42dadaf4b31bd3b39646431
Corneliu <73847818+muffindud@users.noreply.github.com>
2024-12-14 02:25:56 +0200

    Feat/etl warehouse (#40)
    * add warehouse docker compose config
    
    * etl service initial commit
    
    * adapt user manager db for etl
    
    * adapt exch service transfer db for warehouse
    
    * adapt game lobby log db for wh
    
    * remove variable field
    
    * add message split
    
    * add user id get func
    
    * implement data warehouse and etl
    
    ---------
    
    Co-authored-by: buffaloss <beatricia.golban@isa.utm.md>

---
+++ etl-service/src/user_manager.py
@@ -0,0 +1,41 @@
+import psycopg
+
+
+class PostgresConn:
+    def __init__(self, host, port, user, password, dbname):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.dbname = dbname
+
+        self.conn = psycopg.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            dbname=self.dbname,
+        )
+
+    def get_last_id(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT MAX(id) FROM users")
+        return cursor.fetchone()[0]
+
+    def check_if_updated(self, last_id):
+        return self.get_last_id() != last_id
+
+    def get_data_since_last_id(self, last_id):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT id, username, email FROM users WHERE id > {last_id}")
+        return cursor.fetchall()
+
+    def get_all_data(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT id, username, email FROM users")
+        return cursor.fetchall()
+
+    def get_user_id(self, username):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT id FROM users WHERE username = '{username}'")
+        return cursor.fetchone()[0]
